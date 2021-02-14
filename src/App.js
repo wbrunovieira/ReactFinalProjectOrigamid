48683a162a565b71f859a8aebe286b162bc01e5a
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-14 22:28:49 +0000

    made the structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import './App.css';
-import Api from './Api/api';
+
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div><Api /></div>;
+  return <div>
+    
+    <BrowserRouter>
+    <Header />
+
+    <Routes>
+      
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      
+     </Routes> 
+
+    <Footer />
+    </BrowserRouter>
+
+    
+
+    </div>;
 };
 
 export default App;
