603505f04a4a8027b0c6d05b709b8ffa4d049e47
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-15 18:18:52 +0000

    created login routes and LoginForm

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     <Routes>
       
       <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
+      <Route path="/login/*" element={<Login />}/>
       
      </Routes> 
 
