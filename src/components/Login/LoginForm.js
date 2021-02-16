fd29c7e2562a4face8214221fe68508a3a9aa415
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-16 22:51:29 +0000

    made form styles, and Hook UseFormm

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 
 const LoginForm = () => {
-const [username, setUsername] = React.useState('');
-const [password, setPassword] = React.useState('');
+    const username = useForm();    
+    const password = useForm();    
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -14,7 +16,7 @@ function handleSubmit(event) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify(),
     })
     .then((response) => {
         console.log(response);
@@ -29,17 +31,10 @@ function handleSubmit(event) {
            Login Form
            <h1>Login</h1>
            <form action="" onSubmit={handleSubmit}>
-               <input
-                type="text"
-                value={username}
-                 onChange={({ target})=>setUsername(target.value)  }/>
-
-          
-               <input
-                type="password"
-                value={password}
-                 onChange={({ target})=>setPassword(target.value)  }/>
-                 <button>Entrar</button>
+               <Input label="Usuario" type="text" name="username" { ...username }/>
+               <Input label="Senha" type="password" name="password" { ...password }/>
+               
+                <Button>Entrar</Button>
 
            </form>
            <Link to="/login/criar">Cadastro</Link> 
