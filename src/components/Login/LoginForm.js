29a6edf081dbdf08ee63d1fa5b767709a503351a
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-21 13:31:17 +0000

    crated a new rook, use form, and created a regex for email for login

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -11,24 +11,29 @@ const LoginForm = () => {
 
 function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(),
-    })
-    .then((response) => {
-        console.log(response);
-        return response.json();
-    })
-    .then((json) => {
-        console.log(json);
-    });
+
+    if(username.validate() && password.validate()){
+
+
+        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(),
+        })
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+        });
+    }
 }
     return (
         <section>
-           Login Form
+           
            <h1>Login</h1>
            <form action="" onSubmit={handleSubmit}>
                <Input label="Usuario" type="text" name="username" { ...username }/>
